273d9400eb4b5c66a60af16b62af90d964b85e56
Nowko <magoga2896@gmail.com>
2024-09-29 17:54:39 +0200

    ejemplos 23 y 31

--- src/Tema2/Ejemplos/Ejemplo_2_23.kt
+++ src/Tema2/Ejemplos/Ejemplo_2_23.kt
@@ -8,7 +8,10 @@ fun main() {
     var n = f_in.read(buffer)
     while (n != -1) {
         for (i in 0..n - 1) {
-
+            print(buffer[i].toChar())
         }
+        println("")
+        n = f_in.read(buffer)
     }
+    f_in.close()
 }
