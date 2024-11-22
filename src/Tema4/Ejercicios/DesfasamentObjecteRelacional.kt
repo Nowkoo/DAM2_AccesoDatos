6def1e903d7e18cb295e05e9990d3d1e50129cde
mgg <margomgar35@alu.edu.gva.es>
2024-11-22 19:36:13 +0100

    acabat: ejercicis finals tema 4

---
+++ src/Tema4/Ejercicios/DesfasamentObjecteRelacional.kt
@@ -0,0 +1,25 @@
+package Tema4.Ejercicios
+
+data class Comarca(
+    val provincia: String,
+    val nom: String
+)
+
+data class Poblacio(
+    val nom: String,
+    val poblacio: Int,
+    val extensio: Double,
+    val altura: Int,
+    val longitud: String,
+    val latitud: String,
+    val llengua: String,
+    val comarca: Comarca //cod_c
+)
+
+data class Institut(
+    val adreca: String,
+    val codpostal: Int,
+    val nom: String,
+    val numero: String,
+    val poblacio: Poblacio //cod_m
+)
